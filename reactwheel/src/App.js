a14eb93063e06cea0dd62b938a5b0d62bc1c94d0
StenStenNazar <naziksten@gmail.com>
2022-01-27 19:49:00 +0200

    hw-4

--- reactwheel/src/App.js
+++ reactwheel/src/App.js
@@ -1,13 +1,77 @@
+import {useReducer} from "react";
 
-import './App.css';
-import Users from "./components/Users/Users";
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return {...state, count1: state.count1 + 1}
+        case 'decrement':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: state.count1 = 0}
+
+        case 'increment2':
+            return {...state, count2: state.count2 + 1}
+        case 'decrement2':
+            return {...state, count2: state.count2 - 1}
+        case 'reset2':
+            return {...state, count2: state.count2 = 0}
+
+        case 'increment3':
+            return {...state, count3: state.count3 + 1}
+        case 'decrement3':
+            return {...state, count3: state.count3 - 1}
+        case 'reset3':
+            return {...state, count3: state.count3 = 0}
+    }
+}
 
 function App() {
-  return (
-    <div>
-      <Users/>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0, count3: 0});
+    return (
+        <div>
+            <div>{state.count1}</div>
+            <button onClick={() => {
+                dispatch({type: 'increment'})
+            }}>increment
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'decrement'})
+            }}>decrement
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'reset'})
+            }}>recet
+            </button>
+
+            <div>{state.count2}</div>
+            <button onClick={() => {
+                dispatch({type: 'increment2'})
+            }}>increment
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'decrement2'})
+            }}>decrement
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'reset2'})
+            }}>recet
+            </button>
+
+            <div>{state.count3}</div>
+            <button onClick={() => {
+                dispatch({type: 'increment3'})
+            }}>increment
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'decrement3'})
+            }}>decrement
+            </button>
+            <button onClick={() => {
+                dispatch({type: 'reset3'})
+            }}>recet
+            </button>
+        </div>
+    );
 }
 
 export default App;
